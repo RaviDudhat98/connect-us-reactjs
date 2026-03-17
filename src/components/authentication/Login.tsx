e3068143c845171c2cfe01408c8447b12f202cb0
RaviDudhat98 <ravidudhat98@gmail.com>
2026-03-17 20:01:07 +0530

    Changes for Login UI.

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -1,5 +1,66 @@
+import { Box, InputAdornment, TextField } from "@mui/material";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LockOutlineIcon from "@mui/icons-material/LockOutline";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="bg-white border rounded-2xl shadow px-8 py-6 w-[90%] md:w-[40%] ld:w-[30%] ">
+        <h3 className="text-black font-semibold text-2xl">ConnectUs</h3>
+        <span className="text-xs">Sign in to continue</span>
+        <div className="login-container my-3">
+          <Box
+            className="login-form flex flex-col justify-center gap-2"
+            component="form"
+            sx={{
+              "& .MuiTextField-root": {},
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              required
+              className="w-full!"
+              error
+              id="email-input"
+              label="Error"
+              defaultValue="Hello World"
+              helperText="Incorrect entry."
+              autoComplete="off"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailOutlineIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+
+            <TextField
+              required
+              error
+              id="password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              helperText="Incorrect entry."
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockOutlineIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+          </Box>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- src/pages/AuthenticationPage.tsx
+++ src/pages/AuthenticationPage.tsx
@@ -2,9 +2,9 @@ import Login from "../components/authentication/Login";
 
 function AuthenticationPage() {
   return (
-    <>
+    <div className="bg-blue-500 h-screen flex justify-center items-center">
       <Login />
-    </>
+    </div>
   );
 }
 
